a3342ad4adfbd5709dce921acb4ffb40d560d1e6
Taylor-25 <54899165+Taylor-25@users.noreply.github.com>
2019-09-10 09:00:38 -0500

    Add files via upload

---
+++ BigJahn.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class BigJahn here.
+ *
+ * @Taylor Pederson
+ * @version ((9/10/2k19)
+ */
+public class BigJahn
+{
+  public static void main(String[] args){
+       System.out.println("Big Jahn thought 'Hmmm, How did I get here?'");
+       System.out.println("Clarence had died from spider wounds.");
+       System.out.println("Big Jahn was on his last few Mac and Cheese packets.");
+       System.out.println("Out in the middle of the fairy field west of the large think box Big Jahn sat.");
+       System.out.println("He didn't know how to get back home. He just started to walk.");
+       System.out.println("Big Jahn had a long journey to the store and just wanted to go home.");
+       System.out.println("He walked down tree lane, passed the zoom zoom track, and turned down blue 52..");
+       System.out.println("And finally Mac and Cheese and milk in hand he arrived back home safe and sound."); 
+}
+}
